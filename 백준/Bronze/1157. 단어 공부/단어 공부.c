c682dd3b40e0e1ec2e712013b1d43da5cbc7d47a
chhowm <chhowm@gmail.com>
2025-02-24 21:50:46 +0900

    [Bronze I] Title: 단어 공부, Time: 12 ms, Memory: 1972 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.c
+++ 백준/Bronze/1157. 단어 공부/단어 공부.c
@@ -4,42 +4,30 @@
 
 int main() 
 {
-	char salp[26] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
-	char balp[26] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
+	int i;
+	int maxcnt = 0, maxindex = 0, maxflag = 0;;
 	int cnt[26] = { 0 };
-	int i, j;
-	struct max {
-		int maxcnt;
-		int maxindex;
-		int maxflag;
-	}max_;
-	max_.maxcnt = 0;
-	max_.maxflag = 0;
 	char word[1000001];
 	scanf("%s", word);
 
 	for (i = 0; word[i] != '\0'; i++)
 	{
-		for (j = 0; j < 26; j++)
-		{
-			if (word[i] == salp[j] || word[i] == balp[j]) cnt[j]++;
-		}
+		int index = (word[i] >= 'a') ? word[i] - 'a' : word[i] - 'A';
+		cnt[index]++;
 	}
 
 	for (i = 0; i < 26; i++)
 	{
-		if (cnt[i] > max_.maxcnt)
+		if (cnt[i] > maxcnt)
 		{
-			max_.maxcnt = cnt[i];
-			max_.maxindex = i;
+			maxcnt = cnt[i];
+			maxindex = i;
+			maxflag = 0;
 		}
+		else if (cnt[i] == maxcnt) maxflag = 1;
 	}
 
-	for (i = 0; i < 26; i++)
-	{
-		if (cnt[i] == max_.maxcnt)max_.maxflag++;
-	}
-	if (max_.maxflag == 1)printf("%c", balp[max_.maxindex]);
-	else printf("?");
+	if (maxflag) printf("?");
+	else printf("%c", maxindex + 'A');
 	return 0;
 }
